5950370f974448030f9aa28441509ea27ab5d42d
kamp <fbn.kamp@gmail.com>
2026-01-20 19:55:40 +0100

    get similarities debugging

--- scripts/get_similarities.py
+++ scripts/get_similarities.py
@@ -6,33 +6,48 @@ import pandas as pd
 
 results = []
 
-def process(name, obj):
-    if '/' not in name:
-        print("=" * 80)
-    indent = "  " * name.count('/')
-    
-    if isinstance(obj, h5py.Group):
-        print(f"Processing {indent}Group: {name}")
-    elif isinstance(obj, h5py.Dataset):
-        print(f"{indent}Dataset: {name} - shape: {obj.shape}, dtype: {obj.dtype}")
-        get_pair_similarities(name, obj)
+def get_pair_similarities(model_name, group):
+    data = {}
+    prev_id = 0
+    for name, item in group.visititems():    
+        if isinstance(item, h5py.Dataset):
+            img_id, layer = name.split("/")
+            print("\tProcessing ", img_id, layer)
+            
+            assert int(img_id)>=prev_id, "Image order false"
+            prev_id = int(img_id)
 
-def get_pair_similarities(name, dataset):
-    model, set_id, layer = name.split("/")
-    data = dataset[:]
+            assert len(features.shape) == 2, "Feature shape is not 2d"
+            features = item[0,:] # only first replicate
+            
+            if layer not in data.keys(): 
+                data.update({layer: [features]})
+            else: 
+                data[layer].append(features)    
     
-    cor_mat = np.corrcoef(data, rowvar=False)
+    for layer, features in data.items():
+        features = np.vstack(features)
+        corrs = get_corrs(features)
+        for pair_dict in corrs: 
+            pair_dict.update(
+                model = model_name, 
+                layer = layer
+            ) 
+            results.append(pair_dict)
 
-    lower_indices = np.tril_indices(n=cor_mat.shape[0], k=-1)    
-    for i, j in zip(lower_indices[0], lower_indices[1]):
-        results.append({
-            "model": model,
-            "set_id": set_id,
-            "layer": layer,
+def get_corrs(features):  
+    cor_mat = np.corrcoef(features, rowvar=True)
+    assert cor_mat.shape == (4,4), f"Corrmat does not have the shape 4x4."
+
+    indices = np.tril_indices(n=cor_mat.shape[0], k=-1)  
+    corrs = []  
+    for i, j in zip(indices[0], indices[1]):
+        corrs.append({
             "img_a": i,
             "img_b": j,
             "correlation": cor_mat[i, j]
         })
+    return corrs
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Recursively check all groups within h5 file')
@@ -43,7 +58,12 @@ if __name__ == "__main__":
     assert os.path.isfile(args.file), f"{args.file} not found. Check if path is correct."
     with h5py.File(args.file, 'r') as f:
         print("Top-level items:", list(f.keys()))
-        f.visititems(process)
+        
+        for name, item in f.items():
+                print("=" * 80)
+                print(f"Processing Group: {name}")                
+                get_pair_similarities(name, item)
+
     
     assert os.path.isdir(args.output), f"{args.output} is not a directory."
     results = pd.DataFrame(results)
