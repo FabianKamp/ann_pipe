042062223f03876952956c61bbd2c0e173a4c9d4
kamp <fbn.kamp@gmail.com>
2026-01-20 16:35:04 +0100

    debuggin check output

--- scripts/check_output.py
+++ scripts/check_output.py
@@ -1,6 +1,14 @@
 import h5py
+import argparse 
+import os 
 
-with h5py.File('your_file.h5', 'r') as f:
+parser = argparse.ArgumentParser(description='Recursively check all groups withing h5 file')  
+parser.add_argument('--file', type=str, help='Path to the h5 file')  
+args = parser.parse_args()  
+
+assert os.path.isfile(args.file), f"{args.file} not found. Check if path is correct."
+
+with h5py.File(args.file, 'r') as f:
     print("Top-level items:", list(f.keys()))
     def print_groups(name, obj):
         if isinstance(obj, h5py.Group):
